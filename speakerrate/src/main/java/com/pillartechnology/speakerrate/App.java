82ad6d6e96b04c5e2d93a990d9fe998dfd2ede81
Todd Kaufman <toddkaufman@gmail.com>
2010-11-15 11:25:42 -0500

    Initial pom and structure

---
+++ speakerrate/src/main/java/com/pillartechnology/speakerrate/App.java
@@ -0,0 +1,13 @@
+package com.pillartechnology.speakerrate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
